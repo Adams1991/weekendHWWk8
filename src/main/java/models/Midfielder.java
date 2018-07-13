10413b9b8e9d5086fb6d470748b49685957f252f
Adams1991 <adams121@hotmail.co.uk>
2018-07-13 12:50:56 +0100

    midfielder model setup and test setup.

--- src/main/java/models/Midfielder.java
+++ src/main/java/models/Midfielder.java
@@ -1,4 +1,22 @@
 package models;
 
-public class Midfielder {
+public class Midfielder extends Player{
+
+    private double passAccuracy;
+
+    public Midfielder() {
+    }
+
+    public Midfielder(String name, int age, int rating, Team team, double passAccuracy) {
+        super(name, age, rating, team);
+        this.passAccuracy = passAccuracy;
+    }
+
+    public double getPassAccuracy() {
+        return passAccuracy;
+    }
+
+    public void setPassAccuracy(double passAccuracy) {
+        this.passAccuracy = passAccuracy;
+    }
 }
---
+++ src/test/java/models/MidfielderTest.java
@@ -0,0 +1,68 @@
+package models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MidfielderTest {
+
+    Midfielder midfielder;
+    Team team;
+
+
+    public void setUp() {
+        team = new Team();
+        midfielder = new Midfielder("John", 30, 10, team, 30 );
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getAge() {
+    }
+
+    @Test
+    public void setAge() {
+    }
+
+    @Test
+    public void getRating() {
+    }
+
+    @Test
+    public void setRating() {
+    }
+
+    @Test
+    public void getTeam() {
+    }
+
+    @Test
+    public void setTeam() {
+    }
+
+    @Test
+    public void getPassAccurracy(){
+
+    }
+
+    @Test
+    public void setPassAccurracy(){
+
+    }
+}
