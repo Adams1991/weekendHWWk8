b3c7ff4d53037081885a9bcbb18299d4d24b28c3
Adams1991 <adams121@hotmail.co.uk>
2018-07-13 17:16:52 +0100

    all crud functions working on competition

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,8 @@
 import db.DBHelper;
 import models.*;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -10,11 +12,17 @@ public class Runner {
 
         Competition competition = competition = new Competition("Champs Cup", "No Winner", false);
         DBHelper.save(competition);
+        competition.setName("FA Cup");
+        DBHelper.update(competition);
 
-        Competition competition2 = competition = new Competition("Bla Cup", "No Winner", false);
-        DBHelper.save(competition);
-        competition2.setName("FA Cup");
-        DBHelper.update(competition2);
+        Competition competition2 = new Competition("Bla Cup", "No Winner", false);
+        DBHelper.save(competition2);
+        DBHelper.delete(competition2);
+
+        Competition foundCompetition = DBHelper.findById(Competition.class, 1);
+
+        List<Competition> allCompetitions = DBHelper.getAll(Competition.class);
+
 
         Team team = new Team("Cowden FC", 30, competition);
         DBHelper.save(team);
