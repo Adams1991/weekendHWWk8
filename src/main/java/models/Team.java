8c9716f5c62ccd61ed369ab66f6a1e1ab008c01c
Adams1991 <adams121@hotmail.co.uk>
2018-07-13 13:36:01 +0100

    getters and setters setup on Team model. testing next.

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,5 +1,73 @@
 package models;
 
+import java.util.List;
+
 public class Team {
 
+    private int id;
+    private String name;
+    private int points;
+    private List<Player> players;
+    private Manager manager;
+    private Competition competition;
+
+
+    public Team() {
+    }
+
+    public Team(int id, String name, int points, Manager manager, Competition competition) {
+        this.id = id;
+        this.name = name;
+        this.points = points;
+        this.manager = manager;
+        this.competition = competition;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    public Competition getCompetition() {
+        return competition;
+    }
+
+    public void setCompetition(Competition competition) {
+        this.competition = competition;
+    }
 }
