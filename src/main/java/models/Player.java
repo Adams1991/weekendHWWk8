2614345a5e13adc63bc3a8ca597ae209bb70a6f6
Adams1991 <adams121@hotmail.co.uk>
2018-07-13 11:43:30 +0100

    player abstract class setup

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,4 +1,61 @@
 package models;
 
-public class Player {
+public abstract class Player {
+    private int id;
+    private String name;
+    private int age;
+    private int rating;
+    private Team team;
+
+    public Player() {
+    }
+
+    public Player(String name, int age, int rating, Team team) {
+        this.name = name;
+        this.age = age;
+        this.rating = rating;
+        this.team = team;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+
 }
